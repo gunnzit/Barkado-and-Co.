7d38a50a96b070c609971d39bae9edc34d518705
Gunnzit <gunnzit@gmail.com>
2026-08-09 21:27:28 +0530

    Restore env setup, seed accessories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,8 @@
+import path from "path";
+import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 const prisma = new PrismaClient();
 
