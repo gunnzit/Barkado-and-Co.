76143a2b759cc4d7e43f0761c6b0bae103ca26ac
Gunnzit <gunnzit@gmail.com>
2026-08-11 16:34:27 +0530

    Allow Vercel Blob storage domain for Next.js Image component

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
 module.exports = nextConfig;
